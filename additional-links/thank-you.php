e3d010844a47cf739d056b5689a061c74ef385fc
Emine Shaban <emineshaban@icloud.com>
2024-04-18 08:15:33 +0300

    .

--- additional-links/thank-you.php
+++ additional-links/thank-you.php
@@ -14,43 +14,54 @@
             display: flex;
             justify-content: center;
         }
-        
+
         div.image-product {
-            margin: 5vh;
-            width: 50%;
+            margin: auto;
+            width: 70%;
         }
-        
+
         div.image-product img {
-            width: 80%;
+            width: 100%;
         }
-        
+
         div.div-wrapper {
             margin: 5vh;
-            display: flex;
-            justify-content: right;
+            display: grid;
+            grid-template-columns: 0.7fr 1fr;
+            width: 80%;
+            margin: auto;
+            margin-top: 40px;
         }
-        
-        div.header-name {
-            margin-top: 11vh;
+
+        div.header-name { 
             font-size: 2vh;
             width: 50%;
+            margin: auto;
+        }
+
+        h2 {
+            color: #0c1579;
+            font-weight: 800;
+            font-size: 35px;
+            text-shadow: #0c157968 1px 0 10px;
         }
     </style>
 </head>
 
 <body>
     <div class="image-logo-1">
-        <img src="../images/logo.jpg" class="logo-1" />
+        <h2>FungEasy</h2>
+
     </div>
     <hr />
     <div class="div-wrapper">
 
         <div class="image-product">
-            <img src="../images/section3.jpg" class="product" />
+            <img src="../images/product.webp" class="product" />
         </div>
 
         <div class="header-name">
-            <p class="name-product">DERMA E Advanced Peptides and Collagen Moisturizer</p>
+            <p class="name-product">FungEase</p>
             <p class="name-product-text">Thank you for your order, it has been successfully received!</p>
             <p class="name-product-text-bold">Order details: </p>
             <p class="name-product-text"><i class="far fa-check-circle"></i> Order Number: #524956</p>
